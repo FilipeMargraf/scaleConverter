34b7611923dd18e063d5f3582a09e1f070cecf17
Filipe Margraf <filipemargraf1@gmail.com>
2021-08-13 19:08:46 -0300

    Ajuste do layout e finalização da lógica

--- src/script.js
+++ src/script.js
@@ -4,33 +4,51 @@ function convert(event) {
     var inputValue = document.getElementById("scaleInput").value;
     var outputValue = document.getElementById("scaleOutput").value;
     var result = tempValue;
+    let correction = document.getElementById('correction');
 
     if (inputValue == "Kelvin" && outputValue == "Kelvin" || inputValue == "Celsius" && outputValue == "Celsius" || inputValue == "Fahrenheit" && outputValue == "Fahrenheit") {
-        result = tempValue + "K"
+        result = tempValue;
         document.getElementById('scaleInput').style.border = '2px solid #FF0000';
         document.getElementById('scaleOutput').style.border = '2px solid #FF0000';
-        let correction = document.getElementById('correction');
         correction.innerHTML = `<p><strong>Mude as escalas para obter resultado diferente!</strong></p>`;
     }
     else if (inputValue == "Kelvin" && outputValue == "Celsius") {
-        result = (273 - tempValue) + "°C"; 
+        result = (273 - tempValue) + "°C";
+        document.getElementById('scaleInput').style.border = "1px solid #000";
+        document.getElementById('scaleOutput').style.border = "1px solid #000";
+        correction.innerHTML = ``;
     }
     else if (inputValue == "Kelvin" && outputValue == "Fahrenheit") {
         result = (tempValue - 273) * 1.8 + 32 + "°F";
+        document.getElementById('scaleInput').style.border = "1px solid #000";
+        document.getElementById('scaleOutput').style.border = "1px solid #000";
+        correction.innerHTML = ``;
     }
     else if (inputValue == "Celsius" && outputValue == "Kelvin") {
         result = (tempValue + 273) + "K";
+        document.getElementById('scaleInput').style.border = "1px solid #000";
+        document.getElementById('scaleOutput').style.border = "1px solid #000";
+        correction.innerHTML = ``;
     }
     else if (inputValue == "Celsius" && outputValue == "Fahrenheit") {
         result = tempValue * 1.8 + 32 + "°F";
+        document.getElementById('scaleInput').style.border = "1px solid #000";
+        document.getElementById('scaleOutput').style.border = "1px solid #000";
+        correction.innerHTML = ``;
     }
     else if (inputValue == "Fahrenheit" && outputValue == "Kelvin") {
-        result = (tempValue - 32) * (5/9) + 273 + "K";
+        result = (tempValue - 32) * (5 / 9) + 273 + "K";
+        document.getElementById('scaleInput').style.border = "1px solid #000";
+        document.getElementById('scaleOutput').style.border = "1px solid #000";
+        correction.innerHTML = ``;
     }
     else if (inputValue == "Fahrenheit" && outputValue == "Celsius") {
         result = (tempValue - 32) / 1.8 + "°C"
+        document.getElementById('scaleInput').style.border = "1px solid #000";
+        document.getElementById('scaleOutput').style.border = "1px solid #000";
+        correction.innerHTML = ``;
     }
-    
+
 
     document.getElementById("tempResult").value = result;
 }
